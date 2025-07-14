a8ab68ba88c4314fe9f01f741940217b180e3a7d
AmanT776 <amantes776@gmail.com>
2025-07-15 00:37:30 +0300

    fixed type errors

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 exports.signUp = async(req,res,next)=>{
     try{
         const {name,role,email,password} = req.body;
-        // const exists = await User.findOne({email});
-        // if(exists) return res.status(409).json({message: "user already exists"});
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
         const user = await User.create({name,role,email,password: hash});
